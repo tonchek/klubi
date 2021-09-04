5a8fc6f0d31f41b1c343316e6a6d5046f3e86705
Toni Lukenda <toni.lukenda@gmail.com>
2021-09-04 11:52:42 +0200

    osnovni design

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once "header.php";
+?>
+
+<a href="uporabniki.php">Uporabniki</a>
+
+<?php
+include_once "footer.php";
+?>
---
+++ uporabniki.php
@@ -0,0 +1,9 @@
+<?php
+include_once "header.php";
+?>
+
+<a href="index.php">Domov</a>
+
+<?php
+include_once "footer.php";
+?>
