e2ffd3a4b874d02371181ac7a2a6223dd790b173
Toni Lukenda <toni.lukenda@gmail.com>
2021-09-06 23:10:22 +0200

    Stran vnese grb kluba

--- club_add.php
+++ club_add.php
@@ -11,7 +11,7 @@
             <div class="row">
                 <div class="col-lg-6 mx-auto">
                     <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
-                    <form action="club_insert.php" method="post">
+                    <form action="club_insert.php" method="post" enctype="multipart/form-data">
                         <div class="control-group">
                             <div class="form-group floating-label-form-group controls mb-0 pb-2">
                                 <label>Ime kluba</label>
--- club_insert.php
+++ club_insert.php
@@ -4,11 +4,38 @@ include_once "database.php";
  
 $klub = $_POST['klub'];
 $opis = $_POST['opis'];
-//$grb = $_POST['grb'];
-$grb = 'grb';
 
-if(!empty($klub) && !empty($grb)){
+$target_dir = "grbi/";
 
+$random = date('Ymdhisu');
+
+$grb = $target_dir . $random . basename($_FILES["grb"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($grb,PATHINFO_EXTENSION));
+
+//preveri ali datoteka ima dejansko velikost
+$check = getimagesize($_FILES["grb"]["tmp_name"]);
+if($check !== false) {
+    $uploadOk = 1;
+  } else {
+    $uploadOk = 0;
+  }
+
+
+// check file size max 5 MB
+if ($_FILES["grb"]["size"] > 5000000) {
+    $uploadOk = 0;
+  }
+
+// allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+    $uploadOk = 0;
+  }
+
+if(!empty($klub) && ($uploadOk == 1)){
+
+    if (move_uploaded_file($_FILES["grb"]["tmp_name"], $grb)) {
     $query = "INSERT INTO klubi(klub,opis,grb) VALUES (?,?,?)";
 
     $stmt = $pdo->prepare($query);
@@ -16,6 +43,10 @@ if(!empty($klub) && !empty($grb)){
 
     header("Location: clubs.php");
     die();
+} else{
+    header("Location: club_add.php");
+    die();
+}
 }
 else {
     header("Location: club_add.php");
